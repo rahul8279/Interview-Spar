6d4fba26d160ba1ea831fb4e8850d711ec09b3a9
rahul8279 <rana827965@gmail.com>
2025-08-07 13:54:06 +0530

    final

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,5 +1,5 @@
 
-const BASE_URL = "http://localhost:8000/api/v1"
+const BASE_URL = "https://interview-spar.onrender.com/api/v1"
 export const Auth_BASE_URL = `${BASE_URL}/user`
 export const SESSION_BASE_URL = `${BASE_URL}/session`
 export const QUESTION_BASE_URL = `${BASE_URL}/question`
